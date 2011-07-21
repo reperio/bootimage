c78f3a53452113cb3f47499ab208738beb2aa8ac
Kyle Sheumaker <ksheumaker@advancedclustering.com>
2011-07-21 09:54:20 -0500

    Don't be silly and overwrite .so that come from the packages with ones that come from the system

--- findso.pl
+++ findso.pl
@@ -44,5 +44,16 @@ for my $i (@exefiles) {
 }
 
 foreach my $library (sort keys %libs) {
-	system("cp -v --parents $library $PATH");
+
+	my $lib_basename = `basename $library`;
+	chomp $lib_basename;
+	my @results = `find $PATH -name $lib_basename -print`;
+	my $result_count = @results;
+
+	if ($result_count >= 1) {
+		print "Found $lib_basename already in initrd skipping.\n";
+	}
+	else {
+		system("cp -v --parents $library $PATH");
+	}
 }
