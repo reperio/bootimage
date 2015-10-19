c84e3b9c53053bfdb5b1a088e7e4e91cc2c62c79
Kyle Sheumaker <ksheumaker@advancedclustering.com>
2015-10-19 16:30:23 -0500

    Updated build.sh to always include libstdc++ and libgomp from /usr/lib64 instead of pulling from /act/gcc-*/lib64

--- build.sh
+++ build.sh
@@ -189,6 +189,8 @@ echo -e "${ANSI_LEFT}${ANSI_GREEN}[ OK ]${ANSI_DONE}"
 # add a few additional manually
 
 ADDLLIBS="\
+/usr/lib64/libstdc++.so.6 \
+/usr/lib64/libgomp.so.1 \
 ld-linux-x86-64.so.2 \
 libnss_compat.so.2 \
 libnss_dns.so.2 \
